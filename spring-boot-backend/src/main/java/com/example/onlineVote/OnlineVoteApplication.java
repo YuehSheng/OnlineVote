d29fa77e75b0525e943364d8e62a167e8b528afb
ZYS <0228zys@gmail.com>
2024-01-12 20:55:58 +0800

    springboot append

---
+++ spring-boot-backend/src/main/java/com/example/onlineVote/OnlineVoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineVote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineVoteApplication.class, args);
+	}
+
+}
---
+++ spring-boot-backend/src/main/resources/DB/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO vote_items (name) VALUES ('藍');
+INSERT INTO vote_items (name) VALUES ('白');
+INSERT INTO vote_items (name) VALUES ('綠');
